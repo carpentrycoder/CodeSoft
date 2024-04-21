18ca1ee8bd4fa29fceb3367991cf45771a83ddf3
Sutar <nikhilsutar621@gmail.com>
2024-04-21 11:15:10 +0530

    started developing frontend
    navbar created within tailwind css

---
+++ Sutar.ki.Dukaan/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar/> {/* Render Navbar component */}
+    </>
+  );
+}
+
+export default App;
