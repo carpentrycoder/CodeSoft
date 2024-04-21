0a14656c3f3c639bcb860db757080c2dffc3ea5c
Sutar <nikhilsutar621@gmail.com>
2024-04-21 08:02:52 +0530

    simultaneous running of frontend and backend
    adding : nodemon and concurrently npm libraries

--- Sutar.ki.Dukaan/backend/index.js
+++ Sutar.ki.Dukaan/backend/index.js
@@ -1,2 +1,15 @@
 const express = require("express");
 
+const app =  express()
+
+app.listen(
+    4000,
+    () => console.log("backend is running")
+)
+
+app.get(
+    "/",
+    (req,res)=>{
+        res.send("hello");
+    }
+)
