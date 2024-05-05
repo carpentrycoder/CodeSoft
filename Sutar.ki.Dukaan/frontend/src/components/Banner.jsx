1c375c77557aa875afd42b8c865e4f62b2a3fb30
Sutar <nikhilsutar621@gmail.com>
2024-05-05 13:16:09 +0530

    initial update

--- Sutar.ki.Dukaan/frontend/src/components/Banner.jsx
+++ Sutar.ki.Dukaan/frontend/src/components/Banner.jsx
@@ -15,14 +15,14 @@ export default function Banner() {
   return (
     <section>
       <div className="bg-black text-white">
-        <div className="container mx-auto flex flex-col md:flex-row items-center my-9 -mx-10 md:my-24">
+        <div className="container  flex flex-col md:flex-row items-center my-9 -mx-10 md:my-24">
           <div className="flex flex-col w-full lg:w-1/3 justify-center items-start p-6">
             <h1 className="text-3xl md:text-5xl p-2 text-yellow-300 tracking-loose">Sutar.Ki.Dukaan</h1>
             <h2 className="text-3xl md:text-3xl leading-relaxed md:leading-snug mb-2">Your One-Stop Shop for Products</h2>
             <p className="text-sm md:text-base text-gray-50 mb-4">Explore your favourite clothings and register now and explore your Dressing Sense.</p>
             <div className="flex flex-wrap">
-              <button className="transition duration-700 ease-in-out bg-transparent hover:bg-yellow-300 text-yellow-300 font-bold hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border-5 border-yellow-300 hover:bg-white mb-2 md:mr-2" onClick={handleLoginClick}>Login</button>
-              <button className="transition duration-700 ease-in-out bg-transparent hover:bg-yellow-300 text-yellow-300 font-bold hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border-5 border-yellow-300 hover:bg-white mb-2" onClick={handleSignupClick}>Signup</button>
+              <button className="transition duration-700 ease-in-out bg-transparent  text-yellow-300 font-bold hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border-5 border-yellow-300 hover:bg-white mb-2 md:mr-2" onClick={handleLoginClick}>Login</button>
+              <button className="transition duration-700 ease-in-out bg-transparent  text-yellow-300 font-bold hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border-5 border-yellow-300 hover:bg-white mb-2" onClick={handleSignupClick}>Signup</button>
             </div>
           </div>
           <div className="flex justify-center items-center flex-wrap w-full lg:w-2/3 mt-10 md:mt-0">
--- Sutar.ki.Dukaan/frontend/src/components/Shop.jsx
+++ Sutar.ki.Dukaan/frontend/src/components/Shop.jsx
@@ -8,7 +8,7 @@ function Shop() {
   return (
     <>
       <Navbar/>
-      <img src="https://i.postimg.cc/vT8Vhbcj/sutarkidukan.png" alt="sutarkidukan" className="pt-3 bg-clip-border mx-auto object-fill h-1000 w-3000" />
+      <img src="https://i.postimg.cc/R0r34TTj/sutarkidukan-1.png" alt="sutarkidukan" className="pt-3 bg-clip-border mx-auto object-fill h-1000 w-3000 " />
       <Cards />
       <Footer />
     </>
